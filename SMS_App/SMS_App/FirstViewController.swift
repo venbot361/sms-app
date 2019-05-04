c43871bbbd4d416d376703bacb11a4506f0973bc
Vaageesha Das <vaageesha.das@gmail.com>
2019-05-03 21:46:15 -0400

    Setup the app

---
+++ SMS_App/SMS_App/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SMS_App
+//
+//  Created by Minakshi on 5/3/19.
+//  Copyright © 2019 Venkat. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SMS_App/SMS_AppTests/SMS_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  SMS_AppTests.swift
+//  SMS_AppTests
+//
+//  Created by Minakshi on 5/3/19.
+//  Copyright © 2019 Venkat. All rights reserved.
+//
+
+import XCTest
+@testable import SMS_App
+
+class SMS_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
