f7fa125a3dfd5a21836e8648fa17730865168933
Vaageesha Das <vaageesha.das@gmail.com>
2019-05-06 18:18:45 -0400

    Calendar

--- SMS_App/SMS_App/SecondViewController.swift
+++ SMS_App/SMS_App/SecondViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import WebKit
 
 class SecondViewController: UIViewController {
-
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadWebContent()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func loadWebContent() {
+        if let myURL = URL(string: "http://mohigans.mono.k12.wv.us/ical.ashx?e=&s=6&t=null&sd=5/1/2019&ed=4/30/2020&wk=&n=Events&v=&l=&dee=true") {
+            let myRequest = URLRequest(url: myURL)
+            webView?.load(myRequest)
+        }
     }
 
     override func didReceiveMemoryWarning() {
